4ed144aa20469abcfc22d7ed00afe70ae4d31781
Loxenary <110291644+Loxenary@users.noreply.github.com>
2023-04-20 00:39:45 +0700

    Update MainProgram.py

--- TubesDaspro/MainProgram.py
+++ TubesDaspro/MainProgram.py
@@ -1,3 +1,4 @@
+# import semua module untuk dipanggil fungsinya
 import Login as Login
 import Logout as Logout
 import BuildInFunction as Build
@@ -15,74 +16,96 @@ import kokok as AyamBerkokok
 import save as Saving
 import Help as helpPlayer
 import exit as ExitGame
+# import os untuk clear command sehingga terlihat lebih bersih
 import os 
+# import time untuk memberikan jeda sehingga terkesan lebih pas
 import time
-def cekLoginState():
-    print("e")
+def cekLoginState(): # berfungsi untuk mengecek keadaan user apakah sudah login atau belum, jika sudah akan return True jika belum akan return false
     os.system('cls')
     time.sleep(0.25)
-    if(data.LoginState == True):
+    if(data.LoginState == True): # data.LoginState adalah variable yang diubah ubah berdasarkan keadaan login user saat itu
         return True
     else:
         print("User Harus Login Terlebih dahulu")
         print('Ketik "login" pada pilihan untuk login terlebih dahulu\n')
         return False
-def cekAksesUser(user_role,functionName):
-    if(data.role_user == user_role):
+
+# cek akses user berfungsi untuk men-check apakah user saat itu dapat mengakses fitur tertentu pada sistem, akan me-return true jika role nya benar, dan return false jika tidak benar
+def cekAksesUser(user_role,functionName): # user_role adalah role user apa yang boleh mengakses fitur tersebut, functionName adalah nama fitur atau fungsi yang dimaksud
+    if(data.role_user == user_role): # data.role_user adalah role user saat itu, misal: jika login sebagai Bondowoso maka role nya adalah bandung_bondowoso 
         return True
-    else:
-        if(user_role == "bandung_bondowoso" or user_role == "roro_jonggrang"):
+    else: # jika role tidak sesuai
+        if(user_role == "bandung_bondowoso" or user_role == "roro_jonggrang"): 
             print(f"Hanya {user_role} yang dapat Mengakses fitur {functionName}")
             return False
         else:
             print(f"Hanya Jin dengan role {user_role} yang dapat Mengakses fitur {functionName}")
             return False
-def pilihanUser(LoginState,role_user):
-    print("\nMasukkan Pilihan Anda: ")
-    print(data.role_user)
-    pilihan = str(input(">>> "))
-    os.system("cls")
-    if(pilihan == "login" and LoginState == False):
-        Login.UserLogin(data.users)        
-    elif(pilihan == "login" and LoginState == True):
+
+# pilihanUser dibuat sebagai function agar dapat terus dipanggil nantinya hingga permainan berakhir 
+def pilihanUser(LoginState,role_user): # menerima LoginState yaitu keadaan login user saat itu dan role dari user saat itu
+    print("\nMasukkan Pilihan Anda: ") 
+    pilihan = str(input(">>> ")) # menerima input dari user
+
+    os.system("cls") 
+    if(pilihan == "login" and LoginState == False): # fungsi login hanya dapat dipanggil ketika keadaan user sudah logout, dan dapat diakses siapa saja
+        Login.UserLogin(data.users)
+
+    elif(pilihan == "login" and LoginState == True): # jika fungsi login dipanggil saat keadaan user sudah login
         print('Anda sudah Login, pilih command "logout"\nuntuk ganti akun')
-    elif(pilihan == "logout" and cekLoginState() == True):
+
+    elif(pilihan == "logout" and cekLoginState() == True): # fungsi logout hanya dapat dipanggil saat keadaan user sudah login
         Logout.UserLogout()
-    elif(pilihan == "summonjin" and cekLoginState() == True):
-        if(cekAksesUser("bandung_bondowoso",pilihan)):
+
+    # fungsi berikut hanya dapat dipanggil ketika user sudah login dan role user adalah bandung_bondowoso
+    elif(pilihan == "summonjin" and cekLoginState() == True): 
+        if(cekAksesUser("bandung_bondowoso",pilihan)): # role yang dibutuhkan adalah "bandung_bondowoso" dan nama function nya adalah pilihan karena ketika user memasukkan input, input tersebut pasti akan sama dengan function yang dimaksud 
             SummonJin.SummonJin(data.users) 
+
     elif(pilihan == "ubahjin" and cekLoginState() == True):
-        if(cekAksesUser("bandung_bondowoso",pilihan) == True):
+        if(cekAksesUser("bandung_bondowoso",pilihan) == True): 
             UbahJin.FuncUbahJin(data.users) 
+
     elif(pilihan == "hapusjin" and cekLoginState() == True):
         if(cekAksesUser("bandung_bondowoso",pilihan)):
             Hapusjin.HapusJin(data.users,data.candi)
-    elif(pilihan == "kumpul" and cekLoginState() == True):
-        if(cekAksesUser("Pengumpul",pilihan)):
-            pengumpul.JinPengumpul(data.bahan)
-    elif(pilihan == "bangun" and cekLoginState() == True):
-        if(cekAksesUser("Pembangun",pilihan)):
-            pembangun.bangun(data.candi,data.bahan,data.nama_user)
+
     elif(pilihan == "batchkumpul" and cekLoginState() == True):
         if(cekAksesUser("bandung_bondowoso",pilihan)):
             Batch.batch("BatchKumpul",data.users,data.candi,data.bahan)
+
     elif(pilihan == "batchbangun" and cekLoginState() == True):
         if(cekAksesUser("bandung_bondowoso",pilihan)):
             Batch.batch("BatchBangun",data.users,data.candi,data.bahan)
+
     elif(pilihan == "laporanjin" and cekLoginState() == True):
         if(cekAksesUser("bandung_bondowoso",pilihan) == True):
             laporanJin.AmbilLaporanJin(data.users,data.candi,data.bahan) 
+
     elif(pilihan == "laporancandi" and cekLoginState() == True):
         if(cekAksesUser("bandung_bondowoso",pilihan) == True):
              laporanCandi.laporancandi(data.candi)
+
+    # fungsi kumpul hanya dapat diakses jika user sudah login dan user_role adalah Pengumpul         
+    elif(pilihan == "kumpul" and cekLoginState() == True):
+        if(cekAksesUser("Pengumpul",pilihan)):
+            pengumpul.JinPengumpul(data.bahan)
+    # fungsi bangun hanya dapat diakses jika user sudah login dan user_role adalah Pembangun
+    elif(pilihan == "bangun" and cekLoginState() == True):
+        if(cekAksesUser("Pembangun",pilihan)):
+            pembangun.bangun(data.candi,data.bahan,data.nama_user)
+
+    # fungsi berikut hanya dapat diakses jika user sudah login dan user_role adalah roro_jonggrang
     elif(pilihan == "hancurkancandi" and cekLoginState() == True):
         if(cekAksesUser("roro_jonggrang",pilihan)):
             CandiHancur.HancurkanCandi(data.candi)
     elif(pilihan == "ayamberkokok" and cekLoginState() == True):
         if(cekAksesUser("roro_jonggrang",pilihan)):
-            AyamBerkokok.kokokayam(data.candi)   
+            AyamBerkokok.kokokayam(data.candi)
+    # fungsi save dapat dipanggil oleh siapa saja dengan syarat user sudah login
     elif(pilihan == "save" and cekLoginState() == True):
         Saving.save(data.users,data.candi,data.bahan)
+    # fungsi berikut dapat dipanggil oelh siapa saja tanpa syarat
     elif(pilihan == "help"):
         helpPlayer.Help(LoginState,role_user)
     elif(pilihan == "exit"):
@@ -93,8 +116,8 @@ def pilihanUser(LoginState,role_user):
         print(data.candi)
     elif(pilihan == "B"):
         print(data.bahan)
-    else:
+    else: # jika user memasukkan input selain fungsi fungsi diatas
         print("pilihan tidak tersedia")
-    pilihanUser(data.LoginState,data.role_user)
+    pilihanUser(data.LoginState,data.role_user) # untuk melakukan loop tiap kali user selesai menggunakan suatu fitur
 
-pilihanUser(data.LoginState,data.role_user)
+pilihanUser(data.LoginState,data.role_user) # digunakan untuk memulai permainan dengan input user
