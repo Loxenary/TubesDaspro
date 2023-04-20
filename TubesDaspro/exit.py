be60bdf869d92f7ede1b84c892f86c1f26a53551
fislhhhhh <130198566+fislhhhhh@users.noreply.github.com>
2023-04-20 22:43:28 +0700

    Update exit.py

--- TubesDaspro/exit.py
+++ TubesDaspro/exit.py
@@ -1,13 +1,13 @@
 import sys
 import BuildInFunction as Build
-import save as Saving
+import save as Saving # untuk melakukan save progres permainan
 import TempData as data
 def exit ():
     choise = str(input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)"))
     while choise != "y" and choise != "Y" and choise != "N" and choise != "n":
-         choise = str(input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)\n"))
-    if(choise == 'y' or choise == 'Y'):
-        Saving.save(data.users,data.candi,data.bahan)
+         choise = str(input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)\n")) # loop apabila input user tidak sesuai (bukan y atau n)
+    if(choise == 'y' or choise == 'Y'): # user ingin melakukan save terlebih dahulu
+        Saving.save(data.users,data.candi,data.bahan) # untuk melakukan save terlebih dahulu sebelum keluar permainan
         sys.exit("Program Keluar")
     else:
-        sys.exit("Program Keluar")
+        sys.exit("Program Keluar") # langsung keluar permainan tanpa melakukan save
