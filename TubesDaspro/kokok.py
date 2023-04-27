0d1a0786fc9c6e8e4fdeb500e84a975b38ed7cdd
fislhhhhh <130198566+fislhhhhh@users.noreply.github.com>
2023-04-27 23:30:35 +0700

    Update kokok.py

--- TubesDaspro/kokok.py
+++ TubesDaspro/kokok.py
@@ -9,7 +9,7 @@ def kokokayam (candilist) :
             a+=1 # apabila pada candilist telah terisi (bukan [0]), maka variabel jumlah candi bertambah 1
     print("Kukuruyuk")
     print("Jumlah Candi = ",a) # memberi output a sebagai variabel total candi
-    if a==100: # apabila jumlah candi 100 maka bandung bondowoso menang
+    if a==100: # apabila jumlah candi 100 maka bandung bondowoso menjadi pemenangnya
         print("Yah, Bandung Bondowoso memenangkan permainan!")
         sys.exit("Program Keluar") # fungsi kokokayam ini adalah sebagai akhir permainan dan program akan keluar setelahnya
     else : # apabila jumlah candi kurang dari 100 maka roro jonggrang menjadi pemenangnya
