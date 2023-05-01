3b3ff25239057b3d4de46f95b0b29b01c1cf5225
Tim-Buktu <82936135+Tim-Buktu@users.noreply.github.com>
2023-05-01 23:10:33 +0700

    Update save.py

--- TubesDaspro/save.py
+++ TubesDaspro/save.py
@@ -6,11 +6,12 @@ def save():
     userFile_read = open(f'{path}/user.csv','rb')
     candiFile_read = open(f'{path}/candi.csv','rb')
     bahanFile_read = open(f'{path}/bahan_bangunan.csv','rb')
+    #Pembukaan 3 file csv diatas dan dimasukkan ke 3 accesible variabel berbeda
 
-    folder_name = str(input("Masukkan nama folder: "))
+    folder_name = str(input("Masukkan nama folder: "))#Penamaan folder yang ingin kita pakai untuk menyimpan data
     folder_path = os.path.join(parentFolder,folder_name) 
     if(os.path.exists(parentFolder) == False): # parent save/ belum ada
-        os.makedirs(folder_path)    
+        os.makedirs(folder_path)#Pembuatan path parent dan folder kita di dalamnya    
         print("\nSaving...")
         print(f"Membuat folder {parentFolder}")
         print(f"Membuat folder {folder_path}")
@@ -21,7 +22,7 @@ def save():
             print("\nSaving...")
             print(f"\nBerhasil menyimpan data di folder {folder_path}!")
         else: # parent save sudah ada tapi folder belum ada
-            os.makedirs(folder_path,exist_ok=True)
+            os.makedirs(folder_path,exist_ok=True)#Pembuatan folder kita didalam parent folder
             print("\nSaving...")
             print(f"\nBerhasil menyimpan data di folder {folder_path}!")
 
@@ -30,10 +31,12 @@ def save():
     candiFile_write = open(f'{folder_path}/candi.csv',"wb")
     candiFile_write.write(candiFile_read.read())
     bahanFile_write = open(f'{folder_path}/bahan_bangunan.csv',"wb")
-    bahanFile_write.write(bahanFile_read.read())   
+    bahanFile_write.write(bahanFile_read.read())
+    #Rewriting data baru kedalam folder yang baru dibentuk dalam bentuk 3 file csv yang berbeda
     userFile_write.close()  
     candiFile_write.close()
     bahanFile_write.close()
+    #Setelah selesai penulisan file maka file file tersebut ditutup kembali agar progress terupdate di komputer
     override(folder_path)
 
 def override(path):
@@ -47,7 +50,7 @@ def override(path):
     bahanf = open(f'{path}/bahan_bangunan.csv',"w")
     writeFile(bahanf,bahan,4,3)
     
-def writeFile(file,list,PanjangBaris,PanjangKolom):
+def writeFile(file,list,PanjangBaris,PanjangKolom):#Cara penulisan yang readable tiap file difolder tersebut dengan fungsi ini
     for i in range(PanjangBaris):
         if(list[i][0] != None):
             for j in range(PanjangKolom):
