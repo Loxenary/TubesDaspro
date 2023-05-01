a88079bba9aaeba8f3a817f394dd40e8d33493a3
Tim-Buktu <82936135+Tim-Buktu@users.noreply.github.com>
2023-05-01 22:40:00 +0700

    Update Logout.py

--- TubesDaspro/Logout.py
+++ TubesDaspro/Logout.py
@@ -1,4 +1,5 @@
 import TempData as data
 def UserLogout():
     data.LoginState = False
+    #keadaan login dari suatu user dikembalikan menjadi false sehingga user terlogout dari role
     print("Berhasil Logout")
